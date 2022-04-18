6015fcaebd3cab1ff6eba1a34a95037a776f8faa
Benjamin Peck <benjaminlpeck@gmail.com>
2022-04-18 14:53:51 -0700

    completed initial django setup

--- location_django/settings.py
+++ location_django/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.environ['MODE'] == 'dev' else False
 
 ALLOWED_HOSTS = []
 
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'location',
 ]
 
 MIDDLEWARE = [
@@ -76,11 +77,13 @@ WSGI_APPLICATION = 'location_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'locationdb',
+        'USER': 'locationuser',
+        'PASSWORD': 'findme',
+        'HOST': 'localhost',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -117,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+GOOGLE_API_KEY = os.environ['GOOGLE_API_KEY']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
---
+++ settings.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE locationdb;
+CREATE USER locationuser WITH PASSWORD 'findme';
+GRANT ALL PRIVILEGES ON DATABASE locationdb TO locationuser;
