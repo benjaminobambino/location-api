5c3814751610aab4d4d69b94bfa220dea88a1e24
Benjamin Peck <benjaminlpeck@gmail.com>
2022-04-18 18:33:35 -0700

    added models

--- location/models.py
+++ location/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+  address = models.CharField(max_length=255)
+  latitude = models.DecimalField(max_digits=9, decimal_places=7)
+  longitude = models.DecimalField(max_digits=10, decimal_places=7)
+
+
+  def __str__(self):
+    return self.address
+
+class Distance(models.Model):
+  starting_location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='distance')
+  destination = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='distance')
+  distance = models.DecimalField(max_digits=7, decimal_places=2)  
+
+  def __str__(self):
+    return self.distance
