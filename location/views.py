295faa6246fecfe6736871215c84db5856430eb9
Benjamin Peck <benjaminlpeck@gmail.com>
2022-04-23 14:27:01 -0700

    added getLocation view

--- location/views.py
+++ location/views.py
@@ -1,11 +1,22 @@
+# from urllib import request
 from locations.location_api.location_django.settings import GOOGLE_API_KEY, GOOGLE_BASE_URL
 from rest_framework import generics
 from .serializers import LocationSerializer, DistanceSerializer
 from .models import Location, Distance
+import requests
 
-def getLocation(apps):
+def getLocation(request, apps):
   #Model variable
   Location = apps.get_model('location', 'Location')
+  searched_address = request.kwargs['address']
+  query = {'address': searched_address, 'key': GOOGLE_API_KEY}
+  geocode = requests.get(GOOGLE_BASE_URL, params=query)
+  address = geocode.results.formatted_address
+  latitude = geocode.results.geometry.location.lat
+  longitude = geocode.results.geometry.location.lng
+  newLocation = Location(address=address, latitude=latitude, longitude=longitude)
+  newLocation.save()
+  return newLocation
 
 
 # View all locations
