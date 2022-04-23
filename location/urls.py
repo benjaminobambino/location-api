fa4e43fd45a6fbde72be7faec3884d600f8fab02
Benjamin Peck <benjaminlpeck@gmail.com>
2022-04-23 16:55:37 -0700

    added locations urls

--- location/urls.py
+++ location/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 urlpatterns = [
     # URL slugs for API
-    path('location/<address>', views.LocationAdder.as_view(), name='location_list'),
-    # path('colors/<int:pk>', views.ColorDetail.as_view(), name='color_detail'),
+    path('location/<address>', views.LocationAdder.as_view(), name='location_adder'),
+    path('locations/', views.LocationList.as_view(), name='location_list'),
+    path('locations/<int:pk>', views.LocationDetail.as_view(), name='location_detail'),
     # path('swatches/', views.SwatchList.as_view(), name='swatch_list'),
     # path('swatches/<int:pk>', views.SwatchDetail.as_view(), name='swatch_detail'),
 ]
