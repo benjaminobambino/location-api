29cad0a53642536a6da88ce24d6960125e957d41
Benjamin Peck <benjaminlpeck@gmail.com>
2022-04-24 23:24:29 -0700

    updated DistanceFinder url with more clear slug

--- location/urls.py
+++ location/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('location/<address>', views.LocationAdder.as_view(), name='location_adder'),
     path('locations/', views.LocationList.as_view(), name='location_list'),
     path('locations/<int:pk>', views.LocationDetail.as_view(), name='location_detail'),
-    path('distance/<starting>/<ending>', views.DistanceFinder.as_view(), name='distance_finder'),
+    path('distance/<starting>+<ending>', views.DistanceFinder.as_view(), name='distance_finder'),
     path('distances/', views.DistanceList.as_view(), name='distance_list'),
     path('distances/<int:pk>', views.DistanceDetail.as_view(), name='distance_detail'),
 ]
