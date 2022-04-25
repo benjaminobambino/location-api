f34e3c64217f82f0fb85a5c359f8bf43927bd19a
Benjamin Peck <benjaminlpeck@gmail.com>
2022-04-24 23:57:42 -0700

    added static root setting

--- location_django/settings.py
+++ location_django/settings.py
@@ -138,3 +138,5 @@ GOOGLE_BASE_URL = 'https://maps.googleapis.com/maps/api/geocode/json'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT=os.path.join(BASE_DIR, "static/")
